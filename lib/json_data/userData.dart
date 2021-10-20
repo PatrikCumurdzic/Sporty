9998e56fb1c8763753aec625d4214af263b6b9c5
Patrik <patrikcumurdzic@gmail.com>
2021-10-21 00:31:01 +0200

    sitnice

--- lib/json_data/userData.dart
+++ lib/json_data/userData.dart
@@ -2,9 +2,9 @@ class UserData {
   static const myUser = {
     "imagePath":
         'https://resizing.flixster.com/VS2iCmn7Rsk7IQHAT1KBCwZ2oIE=/506x652/v2/https://flxt.tmsimg.com/v9/AllPhotos/57282/57282_v9_bb.jpg',
-    "name": 'Patrik Cumurdzic',
-    "email": 'patrikcumurdzic@gmail.com',
-    "about": 'Pro sam najveci',
+    "name": 'xx',
+    "email": 'xxx',
+    "about": 'xxxxx',
     "rating": 4.2,
   };
 }
